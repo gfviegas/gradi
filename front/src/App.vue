8881d894a3edde4a0e87355f0b1155bcd069ff52
Bruno Marra de Melo <brunomarrademelo@MacBook-Pro-de-Bruno.local>
2019-08-21 10:42:17 -0300

    Starta front-end com arquivos básicos

---
+++ front/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img alt="Logo" src="./assets/logo.png">
+    <HelloWorld msg="Seja bem vindo ao nosso gerenciador de filmes"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style lang="sass">
+  @import "./styles/home.sass"
+</style>
