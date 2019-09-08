a56568092adf06e75ef49e2ddade95bac3a91f9e
Gustavo Viegas <webviegas@gmail.com>
2019-09-08 14:39:27 -0300

    Desabilitando eslint nos scripts JS de scrapping

--- data/scripts/javascript_actors_series.js
+++ data/scripts/javascript_actors_series.js
@@ -1,22 +1,22 @@
 // ref: https://www.w3schools.com/xml/xpath_axes.asp
-
+/* eslint-disable */
 var $max = 20, $i, $stringBuilder = "", $s1, $s2;
 for($i = 1; $i <= $max; $i++){
-	$s1 = document.evaluate("//html/body/div[2]/div/div[2]/descendant::*/tbody/tr[" + ($i*2) + "]/td[2]/a//text()", 
+	$s1 = document.evaluate("//html/body/div[2]/div/div[2]/descendant::*/tbody/tr[" + ($i*2) + "]/td[2]/a//text()",
 		document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent.trim();
 
-	$s2 = document.evaluate("//html/body/div[2]/div/div[2]/descendant::*/tbody/tr[" + ($i*2) + "]/td[4]//node()", 
+	$s2 = document.evaluate("//html/body/div[2]/div/div[2]/descendant::*/tbody/tr[" + ($i*2) + "]/td[4]//node()",
 		document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent.trim();
 	$j = $s2.indexOf("\n");
 	if($j > 0){
 		$s2 = $s2.replace($s2.substring($j), "");
 	}
 	if($s2.length == 0){
-		$s2 = document.evaluate("//html/body/div[2]/div/div[2]/descendant::*/tbody/tr[" + ($i*2) + "]/td[4]/a[1]//text()", 
+		$s2 = document.evaluate("//html/body/div[2]/div/div[2]/descendant::*/tbody/tr[" + ($i*2) + "]/td[4]/a[1]//text()",
 			document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent.trim()
 	}
 
-	$stringBuilder += 
+	$stringBuilder +=
 	"%{name: \""
 	+ $s1
 	+
