253ad06b121020d6fb939501fa542d090ac4e2ba
Gustavo Viegas <webviegas@gmail.com>
2019-09-08 16:21:23 -0300

    tirando Id do spinoff

--- api/lib/gradi_graphql/types/series_spinoff.ex
+++ api/lib/gradi_graphql/types/series_spinoff.ex
@@ -2,7 +2,6 @@ defmodule GradiGraphql.Schema.SeriesSpinoff do
   use Absinthe.Schema.Notation
 
   object :series_spinoff do
-    field :id, :id
     field :imdb_id, :id
     field :title, :string
   end
