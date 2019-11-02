dbf96b0e760ec8d9d8b30cb5a7a8599186c212bd
Vítor Luís <vitorluisgs@gmail.com>
2019-11-02 20:07:02 -0300

    corrige bug das series

--- api/lib/gradi_web/controllers/series_controller.ex
+++ api/lib/gradi_web/controllers/series_controller.ex
@@ -1,11 +1,11 @@
-# Controlador do upload dos filmes
+# Controlador do upload das séries
 defmodule GradiWeb.SeriesController do
   use GradiWeb, :controller
   alias GradiValidation.XMLSchema
 
   def series(conn, %{"series" => file}) do
     IO.inspect file.path
-    case XMLSchema.validate(file.path, :movie) do
+    case XMLSchema.validate(file.path, :series) do
       # TODO: pegar o novo arquivo e fazer o resto do fluxo.
       {:ok, new_file} -> conn |> send_resp(201, "OK!")
       {:error, _} -> conn |> send_resp(409, "Erro")
