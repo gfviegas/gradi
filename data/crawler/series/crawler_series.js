8c177ae117a6a749f160546a4bc03ae4b53481dc
Daniel Freitas <daniel.f.martins@ufv.br>
2019-11-02 11:51:49 -0300

    Crawler indo (50%)

--- data/crawler/series/crawler_series.js
+++ data/crawler/series/crawler_series.js
@@ -1,29 +1,108 @@
 var req = require('request');
 var che = require('cheerio');
 var fs = require('fs');
+eval(fs.readFileSync('javascript_ombd_series.js'));
+var jos = require('./javascript_ombd_series');
 
-console.log('--- INICIANDO ---');
+function craw(callback){
+	console.log('--- INICIANDO ---');	
 
-req('http://www.imdb.com/chart/moviemeter', function(err, res, body){
-	if(err)
-		console.log('Erro: ' + err);
-	
-	var $body = che.load(body);
-	var $seriesset = '<seriesset>' + '\n';
-	var e = '  ';
-	$body('.lister-list tr').each(function() {
-		var title = $body(this).find('.titleColumn a').text().trim();
-		var rating = $body(this).find('.imdbRating strong').text().trim();
-		$seriesset += e + '<series>\n';
-		$seriesset += e + e + '<imdbid>' + rating + '</imdbid>\n';		
-		$seriesset += e + e + '<title>' + title + '</title>\n';
-		$seriesset += e + '</series>\n';
+	req('https://www.imdb.com/search/keyword/?keywords=series&ref_=fn_al_kw_1', function(err, res, body){
+		if(err){
+			console.log('Erro: ' + err);
+			return;
+		}
+		
+		var $body = che.load(body);
+		//var $seriesset = '<seriesset>' + '\n';
+		//var e = '  ';
+		//$body('.lister-list tr').each(function() {
+			//var title = $body(this).find('.titleColumn a').text().trim();
+			//var rating = $body(this).find('.imdbRating strong').text().trim();
+			//$seriesset += e + '<series>\n';
+			//$seriesset += e + e + '<imdbid>' + rating + '</imdbid>\n';		
+			//$seriesset += e + e + '<title>' + title + '</title>\n';
+			//$seriesset += e + '</series>\n';
+		//});
+			
+		$imdbids = [];
+		$body('.lister-item-header').each(function() {
+			var linkimdb = $body(this).find('a').attr('href');	
+			// /title/tt0087800/?ref_=kw_li_tt
+			var imdbid = linkimdb.substring(7);
+			imdbid = imdbid.substring(0, imdbid.indexOf('/'));
+			$imdbids.push(imdbid);			
+		});
+		
+		callback($imdbids);
+		/*
+		$seriesset += '</seriesset>';
+		fs.writeFile('series_instance_generated.xml', $seriesset, (err) => {
+			if(err)
+				console.log('Erro em fs.appendFile: ' + err);
+			else
+				console.log('--- FIM ---');
+		});*/
 	});
-	$seriesset += '</seriesset>';
-	fs.writeFile('series_instance_generated.xml', $seriesset, (err) => {
+}
+/*
+headers: {
+    'User-Agent': 'MY IPHINE 7s'
+  }
+*/
+function getFromOmdb(imdbid, callback){
+	req('http://www.omdbapi.com/?i=' + imdbid + '&apikey=6f5f8dc6', function(err, res, body){
+		if(err){
+			console.log('Erro 2: ' + err);
+		}
+		callback(err, res, jos.getFromOmdb(body));		
+	});
+}
+
+function testeGetFromOmbd(){
+	var s = getFromOmdb('tt0903747', function(errr, ress, series){
+		if(!errr)
+			fs.writeFile('test.txt', series, (err) => {
+				if(err)
+					console.log('Erro em fs.appendFile: ' + err);
+				else
+					console.log('--- FIM ---');
+			});
+	});
+}
+
+craw(async function($imdbids) {
+	$i = 2;
+	$seriesset = [];
+	$seriessetstring = '<seriesset>' + '\n';
+	$imdbids.forEach((imdbid) => {
+		if($i > 0){
+			$seriesset.push(new Promise((resolve, reject) => {
+				getFromOmdb(imdbid, function(errr, ress, series){
+					if(errr) 
+						reject(errr);
+					resolve(series);
+				});
+			}));		
+		}			
+			
+		$i--;		
+	});	
+	$seriesset = await Promise.all($seriesset);
+	$seriesset.forEach(($series) => {
+		$seriessetstring += $series + '\n';
+	});
+	$seriessetstring += '</seriesset>';
+	fs.writeFile('series_instance_generated.xml', $seriessetstring, (err) => {
 		if(err)
 			console.log('Erro em fs.appendFile: ' + err);
 		else
 			console.log('--- FIM ---');
 	});
 });
+//testeGetFromOmbd();
+
+// Put the User Agent string in lowercase
+//var ua = req.headers;
+// Better to test on mobi than mobile (Firefox, Opera, IE)
+//console.log('-->' + ua);
--- data/crawler/series/javascript_ombd_series.js
+++ data/crawler/series/javascript_ombd_series.js
@@ -1,96 +1,68 @@
 // ref: https://www.w3schools.com/xml/xpath_axes.asp
 /* eslint-disable */
-var $max = 20, $i, $stringBuilder = "", $imdb_id, $title, $release_date = "", $runtime, $classification = "", $genres, $writers = "", $description = "";
-var $languages = "", $countries = "", $poster, $seasons;
 
-$i = 1;
-$title = document.evaluate("//html/body/div/div/div/div[1]/div/div/div[2]/table/tbody/tr[" + $i + "]/td[2]/span/span//text()",
-	document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent.trim().replace(/\"/g, "");
+module.exports = {
+	test: function(){return "Hellow";},
+	getFromOmdb: function(param){
+		var $max = 20, $i, $stringBuilder = "", $imdb_id, $title, $release_date = "", $runtime, $classification = "", $genres, $writers = "", $description = "";
+		var $languages = "", $countries = "", $poster, $seasons;
 
-$i = 4;
-$release_date = document.evaluate("//html/body/div/div/div/div[1]/div/div/div[2]/table/tbody/tr[" + $i + "]/td[2]/span/span/text()",
-	document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent.trim();
+		// $jo = $jsonObject
+		$jo = JSON.parse(param);/*JSON.parse(document.evaluate("//html/body/div/div/div/div[2]/div/div/div[2]/pre//text()",
+			document, null, 9, null).singleNodeValue.textContent.trim());*/
+			
+		$title = $jo.Title;
+		$release_date = $jo.Released;
+		$runtime = $jo.Runtime;
+		$genres = "    <genre>" + $jo.Genre.replace(/, /g, "</genre>\n    <genre>") + "</genre>\n";
+		$writers = "    <writer>" + $jo.Writer.replace(/, /g, "</writer>\n    <writer>") + "</writer>\n";
+		$description = $jo.Plot;
+		$languages = "    <language>" + $jo.Language.replace(/, /g, "</language>\n    <language>") + "</language>\n";
+		$countries = "    <country>" + $jo.Country.replace(/, /g, "</country>\n    <country>") + "</country>\n";
+		$poster = $jo.Poster;
+		$imdb_id = $jo.imdbID;
+		$seasons = $jo.totalSeasons;
 
-$i = 5;
-$runtime = document.evaluate("//html/body/div/div/div/div[1]/div/div/div[2]/table/tbody/tr[" + $i + "]/td[2]/span/span//text()",
-	document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent.trim().replace(/\"/g, "");
+		$release_date = $release_date.split(" ");
+		if($release_date.length == 3){
+			var $d, $m, $a;
+			$d = $release_date[0].replace("\"", "");
+			$m = $release_date[1];
+			if($m == "Jan") $m = "01";
+			else if($m == "Feb") $m = "02";
+			else if($m == "Mar") $m = "03";
+			else if($m == "Apr") $m = "04";
+			else if($m == "May") $m = "05";
+			else if($m == "Jun") $m = "06";
+			else if($m == "Jul") $m = "07";
+			else if($m == "Aug") $m = "08";
+			else if($m == "Sep") $m = "09";
+			else if($m == "Oct") $m = "10";
+			else if($m == "Nov") $m = "11";
+			else if($m == "Dec") $m = "12";
+			$a = $release_date[2].replace("\"", "");
+			$release_date = $a + "-" + $m + "-" + $d;
+		} else{
+			$release_date = "N/A";
+		}
 
-$i = 6;
-$genres = "    <genre>" + document.evaluate("//html/body/div/div/div/div[1]/div/div/div[2]/table/tbody/tr[" + $i + "]/td[2]/span/span//text()",
-	document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent.trim().replace(/\"/g, "")
-	.replace(/, /g, "</genre>\n    <genre>") + "</genre>\n";
-
-$i = 8;
-$writers = "    <writer>" + document.evaluate("//html/body/div/div/div/div[1]/div/div/div[2]/table/tbody/tr[" + $i + "]/td[2]/span/span//text()",
-	document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent.trim().replace(/\"/g, "")
-	.replace(/, /g, "</writer>\n    <writer>") + "</writer>\n";
-
-$i = 10;
-$description = document.evaluate("//html/body/div/div/div/div[1]/div/div/div[2]/table/tbody/tr[" + $i + "]/td[2]/span/span//text()",
-	document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent.trim().replace(/\"/g, "");
-
-$i = 11;
-$languages = "    <language>" + document.evaluate("//html/body/div/div/div/div[1]/div/div/div[2]/table/tbody/tr[" + $i + "]/td[2]/span/span//text()",
-	document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent.trim().replace(/\"/g, "")
-	.replace(/, /g, "</language>\n    <language>") + "</language>\n";
-
-$i = 12;
-$countries = "    <country>" + document.evaluate("//html/body/div/div/div/div[1]/div/div/div[2]/table/tbody/tr[" + $i + "]/td[2]/span/span//text()",
-	document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent.trim().replace(/\"/g, "")
-	.replace(/, /g, "</country>\n    <country>") + "</country>\n";
-
-$i = 14;
-$poster = document.evaluate("//html/body/div/div/div/div[1]/div/div/div[2]/table/tbody/tr[" + $i + "]/td[2]/span/span/a//text()",
-	document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent.trim().replace(/, /g, "\", \"");
-
-$i = 22;
-$imdb_id = document.evaluate("//html/body/div/div/div/div[1]/div/div/div[2]/table/tbody/tr[" + $i + "]/td[2]/span/span//text()",
-	document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent.trim();
-
-$i = 24;
-$seasons = document.evaluate("//html/body/div/div/div/div[1]/div/div/div[2]/table/tbody/tr[" + $i + "]/td[2]/span/span//text()",
-	document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent.trim();
-
-
-$release_date = $release_date.split(" ");
-if($release_date.length == 3){
-	var $d, $m, $a;
-	$d = $release_date[0].replace("\"", "");
-	$m = $release_date[1];
-	if($m == "Jan") $m = "01";
-	else if($m == "Feb") $m = "02";
-	else if($m == "Mar") $m = "03";
-	else if($m == "Apr") $m = "04";
-	else if($m == "May") $m = "05";
-	else if($m == "Jun") $m = "06";
-	else if($m == "Jul") $m = "07";
-	else if($m == "Aug") $m = "08";
-	else if($m == "Sep") $m = "09";
-	else if($m == "Oct") $m = "10";
-	else if($m == "Nov") $m = "11";
-	else if($m == "Dec") $m = "12";
-	$a = $release_date[2].replace("\"", "");
-	$release_date = $a + "-" + $m + "-" + $d;
-} else{
-	$release_date = "N/A";
+		$stringBuilder +=
+		"  <series>\n"
+			+ "    <imdbid>" + $imdb_id + "</imdbid>\n"
+			+ "    <title>" + $title + "</title>\n"
+			+ "    <release_date>" + $release_date + "</release_date>\n"
+			+ "    <runtime>" + $runtime + "</runtime>\n"
+			+ "    <classification>" + $classification + "</classification>\n"
+			+ "    <genres>\n" + $genres + "  </genres>\n"
+			+ "    <writers>\n" + $writers + "  </writers>\n"
+			+ "    <actors>\n@ACTORS  </actors>\n"
+			+ "    <description>" + $description + "</description>\n"
+			+ "    <languages>\n" + $languages + "  </languages>\n"
+			+ "    <countries>\n" + $countries + "  </countries>\n"
+			+ "    <poster>" + $poster + "</poster>\n"
+			+ "    <seasons>" + ($seasons != null ? $seasons : 1) + "</seasons>\n"
+		+
+		"  </series>";
+		return $stringBuilder;		
+	}
 }
-
-$stringBuilder +=
-"<series>\n"
-	+ "  <imdbid>" + $imdb_id + "</imdbid>\n"
-	+ "  <title>" + $title + "</title>\n"
-	+ "  <release_date>" + $release_date + "</release_date>\n"
-	+ "  <runtime>" + $runtime + "</runtime>\n"
-	+ "  <classification>" + $classification + "</classification>\n"
-	+ "  <genres>\n" + $genres + "  </genres>\n"
-	+ "  <writers>\n" + $writers + "  </writers>\n"
-	+ "  <actors>\n@ACTORS  </actors>\n"
-	+ "  <description>" + $description + "</description>\n"
-	+ "  <languages>\n" + $languages + "  </languages>\n"
-	+ "  <countries>\n" + $countries + "  </countries>\n"
-	+ "  <poster>" + $poster + "</poster>\n"
-	+ "  <seasons>" + $seasons + "</seasons>\n"
-+
-"</series>";
-
-console.log($stringBuilder);
